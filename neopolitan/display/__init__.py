c80190a3f74339eb76f7fbbef4966032b6c49c34
Alexi <alexibmost@gmail.com>
2023-05-01 07:07:22 -0700

    lint

--- neopolitan/display/__init__.py
+++ neopolitan/display/__init__.py
@@ -1,2 +1,1 @@
 """Init display module"""
-
--- neopolitan/writing/__init__.py
+++ neopolitan/writing/__init__.py
@@ -2,4 +2,3 @@
 # didn't solve
 # print('importing letters_8, groups_8, and data_transformations from writing')
 # from . import letters_8, groups_8, data_transformation
-
