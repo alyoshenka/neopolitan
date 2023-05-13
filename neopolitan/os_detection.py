60086d788c6e31e0a1cbc10aefd4fc86364b41e2
Alexi <alexibmost@gmail.com>
2023-05-13 08:51:15 -0700

    log maching

--- neopolitan/os_detection.py
+++ neopolitan/os_detection.py
@@ -1,10 +1,11 @@
 """Functions for detecting the operating machine"""
 
 import platform
+from neopolitan.log import get_logger
 
 def on_pi():
     """Is this code being run on a 32 bit Raspberry Pi?"""
-    return (platform.machine()).strip() == 'armv7l'
-
-def test():
-    pass
+    yes = (platform.machine()).strip() == 'armv7l'
+    get_logger().info('On Pi: %s', yes)
+    get_logger().info('Machine: %s', platform.machine())
+    return yes
