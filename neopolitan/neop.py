f887d82c5fee6adff595acd2f059cfcdb458e921
Alexi <38815390+alyoshenka@users.noreply.github.com>
2023-05-14 10:57:41 -0700

    Test package on Pi (#65)
    * python version Pi uses
    
    * platform_machine dependency
    
    * not sure
    
    * init logger
    
    * update version
    
    * dont init logger outside of func
    
    * conditional init logger
    
    * name main issue
    
    * no idea why moduleimporterror
    
    * no idea why it wont run
    
    * encoding issue
    
    * version incr
    
    * log maching
    
    * verify os
    
    * incr version
    
    * log machine
    
    * more logging
    
    * have to install neopixel differently
    
    * logging in testboardrunner
    
    * package prepend
    
    * additional dependencies
    
    * lint

--- neopolitan/neop.py
+++ neopolitan/neop.py
@@ -6,45 +6,31 @@
 # pylint: disable=too-many-branches
 # pylint: disable=import-outside-toplevel
 # pylint: disable=logging-fstring-interpolation
-# ToDo: fix this
-
-# todo: only import pygame if on graphical
+# pylint: disable=too-many-locals
 
 import getopt
 import sys
 import time
-import logging
-import datetime
 
 from neopolitan.board_functions.board import Board
 # from board_functions.colors import OFF, ON
 from neopolitan.board_functions.board_data import default_board_data
 from neopolitan.writing.data_transformation import str_to_data
-from neopolitan.os_detection import on_pi
+from neopolitan.os_detection import on_pi, log_os
+from neopolitan.log import init_logger, get_logger # why tf is this erroring?
 # pylint: disable=wildcard-import
 from neopolitan.const import *
 
-def initialize_logger(prep='logs/'):
-    """Set up the log file"""
-    # todo: prep arg validation
-    import os
-    # make directory if it does not exist
-    # todo: is this ok?
-    if not os.path.exists(prep):
-        os.makedirs(prep)
-    log_time = str(datetime.datetime.now()) \
-        .replace(" ", "_") \
-        .replace(".", "_") \
-        .replace(":", "-")
-    filename = f'{os.getcwd()}/{prep}neopolitan_{log_time}.txt'
-    logging.basicConfig(filename=filename, encoding='utf=8', level=logging.DEBUG)
 
-def main(events=None):
+def main(events=None, initialize_logger=False):
     """Make a very simple display"""
 
-    initialize_logger()
+    if initialize_logger:
+        init_logger()
+    logger = get_logger()
 
     board_data = process_arguments()
+    log_os()
 
     width = WIDTH
     height = HEIGHT
@@ -55,10 +41,12 @@ def main(events=None):
 
     # todo: make better
     if board_data.graphical:
+        logger.info('Initializing graphical display')
         from neopolitan.display.graphical_display import GraphicalDisplay
         board = Board(size)
         display = GraphicalDisplay(board=board)
     else:
+        logger.info('Initializing hardware display')
         from neopolitan.display.hardware_display import HardwareDisplay
         display = HardwareDisplay(WIDTH*HEIGHT)
         board_display = display.board_display
@@ -71,20 +59,20 @@ def main(events=None):
         # todo: make better
         while events and not events.empty():
             event = events.get()
-            logging.info(f'event: {event}')
+            logger.info(f'event: {event}')
             event_list = event.split()
             first = event_list[0]
             if first == 'exit':
                 return
             if first == 'say':
-                logging.info(f'say: {event}')
+                logger.info(f'say: {event}')
                 # todo: better handling: this is unintuitive
                 message = ' '
                 for word in event_list[1:]:
                     message += word + ' '
-                logging.info(message)
+                logger.info(message)
                 board.set_data(str_to_data(message))
-                logging.info(f'set message: {message}')
+                logger.info(f'set message: {message}')
             else: # try board data events
                 board_data = process_board_data_events(board_data, event_list)
             # todo: error handling
@@ -98,6 +86,7 @@ def main(events=None):
 
 def process_arguments():
     """Process the command line arguments and return them as a BoardData object"""
+    logger = get_logger()
     board_data = default_board_data
 
     argument_list = sys.argv[1:]
@@ -112,21 +101,11 @@ def process_arguments():
                     board_data.message = val
                 elif arg in ('-g', '--graphical'):
                     if val == 'True':
-                        if on_pi():
-                            logging.warning('This code cannot be run in graphical' \
-                                            ' mode on a Raspberry Pi, setting graphical to False')
-                            board_data.graphical = False
-                        else:
-                            board_data.graphical = True
+                        board_data.graphical = True
                     elif val == 'False':
-                        if not on_pi():
-                            logging.warning('This code cannot be run in hardware mode when not run'\
-                            ' on a Raspberry Pi, setting graphical to True')
-                            board_data.graphical = True
-                        else:
-                            board_data.graphical = False
+                        board_data.graphical = False
                     else:
-                        logging.warning(f'Could not parse "graphical" argument: {val}')
+                        logger.warning(f'Could not parse "graphical" argument: {val}')
                 elif arg in ('-s', 'scroll'):
                     if val in ('slow', 'medium', 'fast'):
                         board_data.scroll_speed = val
@@ -137,26 +116,38 @@ def process_arguments():
                         else: # fast
                             board_data.scroll_wait = SCROLL_SLOW
                     else:
-                        logging.warning(f'Invalid scroll speed: {val}')
+                        logger.warning(f'Invalid scroll speed: {val}')
                 elif arg in ('-w', 'wrap'):
                     if val == 'True':
                         board_data.should_wrap = True
                     elif val == 'False':
                         board_data.should_wrap = False
                     else:
-                        logging.warning(f'Could not parse "wrap" argument: {val}')
-        logging.info(f'message set to: {board_data.message}')
-        logging.info(f'graphical set to: {board_data.graphical}')
-        logging.info(f'scroll speed set to: {board_data.scroll_speed} ({board_data.scroll_wait})')
-        logging.info(f'wrap set to: {board_data.should_wrap}')
+                        logger.warning(f'Could not parse "wrap" argument: {val}')
+        # --- Verify OS for graphical/hardware
+        if on_pi() and board_data.graphical:
+            logger.warning('This code cannot be run in graphical' \
+                            ' mode on a Raspberry Pi, setting graphical to False')
+            board_data.graphical = False
+        if not on_pi() and not board_data.graphical:
+            logger.warning('This code cannot be run in hardware mode when not run'\
+            ' on a Raspberry Pi, setting graphical to True')
+            board_data.graphical = True
+        # --- Done verifying
+        logger.info(f'message set to: {board_data.message}')
+        logger.info(f'graphical set to: {board_data.graphical}')
+        logger.info(f'scroll speed set to: {board_data.scroll_speed} ({board_data.scroll_wait})')
+        logger.info(f'wrap set to: {board_data.should_wrap}')
 
     except getopt.error as err:
-        logging.error(f'getopt error: {err}')
+        logger.error(f'getopt error: {err}')
 
     return board_data
 
 def process_board_data_events(board_data, event_list):
     """Manipulate board data according to events"""
+
+    logger = get_logger()
 
     first = event_list[0]
     if first == 'speed':
@@ -165,40 +156,40 @@ def process_board_data_events(board_data, event_list):
         # pylint: disable=broad-except
         except Exception as err:
             # todo: better explanation
-            logging.warning('No second value provided, %s', err)
+            logger.warning('No second value provided, %s', err)
         if speed == 'slow':
             board_data.scroll_slow()
-            logging.info('set speed: slow')
+            logger.info('set speed: slow')
         elif speed == 'medium':
             board_data.scroll_medium()
-            logging.info('set speed: medium')
+            logger.info('set speed: medium')
         elif speed == 'fast':
             board_data.scroll_fast()
-            logging.info('set speed: fast')
+            logger.info('set speed: fast')
         else:
             try:
                 speed = float(speed)
                 board_data.set_scroll_wait(speed)
-                logging.info(f'set speed: {speed}')
+                logger.info(f'set speed: {speed}')
             except ValueError:
-                logging.warning(f'Cannot parse speed: {speed}')
+                logger.warning(f'Cannot parse speed: {speed}')
     elif first == 'wrap':
         try:
             wrap = event_list[1]
         # pylint: disable=broad-except
         except Exception as err:
             # todo: better explanation
-            logging.warning('No second value provided, %s', err)
+            logger.warning('No second value provided, %s', err)
         if wrap in ('True', '1'):
             board_data.should_wrap = True
-            logging.info('set wrap: True')
+            logger.info('set wrap: True')
         elif wrap in ('False', '0'):
             board_data.should_wrap = False
-            logging.info('set wrap: False')
+            logger.info('set wrap: False')
         else:
-            logging.warning('Cannot parse wrap: %s', wrap)
+            logger.warning('Cannot parse wrap: %s', wrap)
 
     return board_data
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': # todo: is this still true when running from the thread?
+    main(initialize_logger=True) # try False maybe? for testing
